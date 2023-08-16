55193f56a60f35236539d4bd15259a11422f3a60
Gabor Kovacs <gabor.kovacs@alleninstitute.org>
2023-08-16 16:06:39 -0700

    feat: Add dask array based n5 to multiscale zarr converter
      * Update dependencies.
      * Add manifest file approach instead of command line agruments.
      * Increase block size to approx 8000 chunks.
      * The larger the graph the better the performance.
      * Reduce threads per worker to 2. Prevent memory spilling to tmp space.
      * Add minimal unit tests.
      * Code reformatting.

--- src/aind_exaspim_pipeline_utils/__init__.py
+++ src/aind_exaspim_pipeline_utils/__init__.py
@@ -2,7 +2,9 @@
 """
 from .imagej_macros import ImagejMacros
 from .imagej_wrapper import main
+from .n5tozarr.n5tozarr_da import n5tozarr_da_converter
+from .exaspim_manifest import print_example_manifest
 
-__all__ = ["ImagejMacros", "main"]
+__all__ = ["ImagejMacros", "main", "n5tozarr_da_converter", "print_example_manifest"]
 
 __version__ = "0.2.2"
