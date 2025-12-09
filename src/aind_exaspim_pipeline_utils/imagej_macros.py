67f3ea86f693fcb6b5fb8f8eaf0ea8e8a077dc4a
Cameron Arshadi <arshadic23@gmail.com>
2025-12-09 15:16:56 -0500

    Increase Search radius and reduce inlier factor

--- src/aind_exaspim_pipeline_utils/imagej_macros.py
+++ src/aind_exaspim_pipeline_utils/imagej_macros.py
@@ -115,9 +115,9 @@ run("Detect Interest Points for Registration",
         "interest_points=beads fix_views=[{fix_views}] " +
         "map_back_views=[{map_back_views}] " +
         "transformation={transformation}{regularization} " +
-        "limit_search_radius search_radius=100 " +
+        "limit_search_radius search_radius=600 " +
         "number_of_neighbors=3 redundancy=0 significance=3 " +
-        "allowed_error_for_ransac=30 inlier_factor=6 ransac_iterations=Thorough " +
+        "allowed_error_for_ransac=30 inlier_factor=5 ransac_iterations=Thorough " +
         "global_optimization_strategy="+
         "[One-Round: DO NOT handle unconnected tiles, DO NOT remove wrong links ('classic option')]");
         """
